ac62e59402983a5b26f683aabcc6ac9ff4cc3bcf
milovanovdenis <milovanov-den@bk.ru>
2023-12-02 00:01:40 +0700

    added first lab without a report document

---
+++ first lab/scripts.js
@@ -0,0 +1,40 @@
+var operator = '+'
+var expression = '0 + 0'
+var result
+
+function calculate(){
+    /* получаем из файла index.html данные, введённые пользователем
+    и преобразовываем их к целочисленному типу */
+    first_number = Number(document.getElementsByTagName("input")[0].value)
+    second_number = Number(document.getElementsByTagName("input")[1].value)
+
+    // из-за запрета на if, используем switch - case (аналогом в питоне является match - case)
+    /*switch (operator) {
+        case "+":
+            result = first_number + second_number
+            break
+        case "-":
+            result = first_number - second_number
+            break
+        case "*":
+            result = first_number * second_number
+            break
+        case "/":
+            result = first_number / second_number
+            break
+        case "%":
+            result = first_number % second_number
+            break
+    }*/
+
+    // проводим расчёты и заменяем значение поля "ответ" на полученный результат
+    document.getElementById("result").innerHTML = eval(expression);
+}
+
+// функция получает числа и оператор и склеивает их в одно выражение, в последствии заменяя им значение поля "выражение"
+function create_expression() {
+    first_number = Number(document.getElementsByTagName("input")[0].value)
+    second_number = Number(document.getElementsByTagName("input")[1].value)
+    expression = first_number + " " + operator + " " + second_number
+    document.getElementById('math_expression').innerHTML = expression
+}
